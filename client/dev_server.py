d85748547a9276a541db20259ffd6fd3a5769cf9
Oleg Pshenichniy <olegpshenichniy@gmail.com>
2016-12-16 14:18:56 +0100

    Update client dev server

--- client/dev_server.py
+++ client/dev_server.py
@@ -2,11 +2,14 @@
 import http.server
 import socketserver
 
+HOST = '127.0.0.1'
 PORT = 8000
 
 httpd = socketserver.TCPServer(
-    ("", PORT),
+    (HOST, PORT),
     http.server.SimpleHTTPRequestHandler
 )
+
+print('http://{}:{}'.format(HOST, PORT))
 
 httpd.serve_forever()
--- client/src/js/es6/index.js
+++ client/src/js/es6/index.js
@@ -1,6 +1,4 @@
 import 'jquery'
 import 'bootstrap/dist/js/bootstrap.min.js'
-
 import 'bootswatch/darkly/bootstrap.min.css'
-
 import '../../styles/style.css'
