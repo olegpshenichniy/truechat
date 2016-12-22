654b794347457d7bc6145ee9821782bb7d2f43eb
Oleg Pshenichniy <olegpshenichniy@gmail.com>
2016-12-22 16:26:30 +0100

    Update user model

--- server/api/user/admin.py
+++ server/api/user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+
+class EmployeeInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (EmployeeInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- server/api/user/serializers.py
+++ server/api/user/serializers.py
@@ -1,17 +1,19 @@
-import json
-
-import redis
 from rest_framework import serializers
 
-from django.db.models import Q, Count
 from django.contrib.auth.models import User
-from django.utils import timezone
-from django.conf import settings
 
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'avatar')
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'is_active')
-
+        fields = ('id', 'username', 'first_name', 'last_name', 'is_active', 'profile')
