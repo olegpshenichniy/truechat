09e51f54ef6490bdb48d880ab0551a3d9f11c25e
Oleg Pshenichniy <olegpshenichniy@gmail.com>
2017-03-17 11:07:00 +0100

    Registration process

--- client/src/js/es6/auth.js
+++ client/src/js/es6/auth.js
@@ -11,6 +11,7 @@ class Auth {
 
     this._loginForm = null;
     this._loginButton = null;
+    this._registerForm = null;
     this._registerButton = null;
     this._logOutLink = jQuery('#auth-logout');
 
@@ -41,6 +42,44 @@ class Auth {
                                 </div>
                               </div>
                               <div class="col-lg-4"></div>`;
+
+    this.templateRegistrationForm = `<div class="col-lg-4"></div>
+                                      <div class="col-lg-4">
+                                        <div class="well bs-component">
+                                         <form class="form-horizontal" id="chat-register">
+                                           <fieldset>
+                                             <legend>Registration</legend>
+                                             <div class="form-group">
+                                               <div class="col-lg-12">
+                                                 <input name="username" type="text" class="form-control" placeholder="Username">
+                                               </div>
+                                             </div>
+                                             <div class="form-group">
+                                               <div class="col-lg-12">
+                                                 <input name="email" type="text" class="form-control" placeholder="Email">
+                                               </div>
+                                             </div>
+                                             <div class="form-group">
+                                               <div class="col-lg-12">
+                                                 <input name="password" type="password" class="form-control" placeholder="Password">
+                                               </div>
+                                             </div>
+                                             <div class="form-group">
+                                               <div class="col-lg-12">
+                                                 <input name="password_repeat" type="password" class="form-control" placeholder="Password repeat">
+                                               </div>
+                                             </div>
+                                             <div class="form-group">
+                                               <div class="col-lg-12">
+                                                 <button id="chat-register-button" type="button" class="btn btn-success">Go</button>
+                                                 <button id="chat-login-button" type="button" class="btn btn-default">Login</button>
+                                               </div>
+                                             </div>
+                                           </fieldset>
+                                         </form>
+                                        </div>
+                                      </div>
+                                      <div class="col-lg-4"></div>`;
 
   }
 
@@ -143,7 +182,10 @@ class Auth {
         });
     });
 
-    $this._registerButton.click($this.show_registerForm);
+    $this._registerButton.click(function () {
+      $this.remove_loginForm();
+      $this.show_registerForm();
+    });
   }
 
   remove_loginForm() {
@@ -169,10 +211,73 @@ class Auth {
   }
 
   show_registerForm() {
-    alert('register');
+    if (this._registerForm) {
+      return;
+    }
+
+    let $this = this;
+
+    $this._registerForm = jQuery(this.templateRegistrationForm);
+    $this._registerForm.hide().appendTo(this.app.body).fadeIn();
+    $this._loginButton = jQuery('#chat-login-button');
+    $this._registerButton = jQuery('#chat-register-button');
+
+    $this._loginButton.click(function () {
+      $this.remove_registerForm();
+      $this.show_loginForm();
+    });
+
+    $this._registerButton.click(function () {
+      // hide button, show loader
+      $this._registerButton.hide();
+      $this.app.loader.prepend('register', $this._registerButton.parent());
+
+      // form data
+      let formData = jQuery('#chat-register').serializeArray().reduce(function (m, o) {
+        m[o.name] = o.value;
+        return m;
+      }, {});
+
+      // send xhr and handle deffered
+      $this._register(formData.username, formData.email, formData.password, formData.password_repeat).then(
+        function (data) {
+
+          let respText = data.response;
+          let respStatus = data.status;
+
+          console.log(respStatus);
+
+          $this.app.loader.remove('register', function () {
+            // created
+            if (respStatus === 201) {
+              alert('created');
+            }
+            // warnings
+            else {
+              $this._registerButton.show();
+              $this.app._emitter.emit('auth.register.fail', respText);
+              $this.app.alert.show( 'auth-register', 'warning', jQuery('form', $this._registerForm), Utils.json2message(respText));
+            }
+          });
+        },
+        // error
+        function (err) {
+          $this.app.loader.remove('register', function () {
+            $this._registerButton.show();
+            $this.app.alert.show('auth-register', 'danger', jQuery('form', $this._registerForm), "Something died!");
+          });
+        });
+    });
   }
 
-  remove_registerForm() {}
+  remove_registerForm() {
+    if (this._registerForm) {
+      this._registerForm.remove();
+    }
+    this._loginButton = null;
+    this._registerButton = null;
+    this._registerForm = null;
+  }
 
   _setupToken() {
     if (!this.app.token) {
@@ -218,6 +323,41 @@ class Auth {
   _logout() {
     Utils.setCookie(this.app.tokenCookieKey, '', -1);
     this.app._emitter.emit('auth.logout.success')
+  }
+
+  _register(username, email, password, passwordRepeat) {
+    let xhr = new XMLHttpRequest();
+    let params = jQuery.param({
+      username: username,
+      email: email,
+      password: password,
+      password_repeat: passwordRepeat
+    });
+
+    return new Promise((resolve, reject) => {
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 0) {
+          // unsent
+        } else if (xhr.readyState === 1) {
+          // opened
+        } else if (xhr.readyState === 2) {
+          // headers_received
+        } else if (xhr.readyState === 3) {
+          // loading
+        } else if (xhr.readyState === 4) {
+          // done
+          try {
+            let response = JSON.parse(xhr.responseText);
+            resolve({response: response, status: xhr.status});
+          } catch (err) {
+            reject(err);
+          }
+        }
+      };
+      xhr.open('POST', SETTINGS.api.http.registrationUrl, true);
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhr.send(params);
+    });
   }
 
 }
