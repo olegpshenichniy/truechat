6591df6b10cd94ae48db055eaa11ee98d091d2d4
olegpshenichniy <olegpshenichniy@gmail.com>
2017-06-28 19:41:15 +0200

    Api for private and group chats

--- server/api/thread/serializers.py
+++ server/api/thread/serializers.py
@@ -3,19 +3,20 @@ from rest_framework import serializers
 from .models import PrivateThread, GroupThread
 
 
-class PrivateThreadListCreateSerializer(serializers.ModelSerializer):
+#########################
+##### PrivateThread #####
+#########################
 
+class PrivateThreadListCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrivateThread
         fields = '__all__'
-
 
     def validate_initiator(self, initiator):
         if self.context['request'].user != initiator:
             raise serializers.ValidationError('Current user must be an initiator.')
 
         return initiator
-
 
     def validate_participants(self, participants):
         # participants without initiator
@@ -42,8 +43,75 @@ class PrivateThreadListCreateSerializer(serializers.ModelSerializer):
         return participants
 
 
-class GroupThreadListCreateSerializer(serializers.ModelSerializer):
+class PrivateThreadRetrieveDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PrivateThread
+        fields = '__all__'
 
+
+#######################
+##### GroupThread #####
+#######################
+
+class GroupThreadListCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = GroupThread
         fields = '__all__'
+
+    def validate_owner(self, owner):
+        if self.context['request'].user != owner:
+            raise serializers.ValidationError('Current user must be an owner.')
+
+        return owner
+
+    def validate_name(self, name):
+        if GroupThread.objects.filter(owner=self.context['request'].user, name=name).exists():
+            raise serializers.ValidationError('You already have group chat with this name.')
+
+        return name
+
+    def validate_participants(self, participants):
+        # add initiator by default
+        participants = set(participants) | {self.context['request'].user}
+
+        return participants
+
+
+class GroupThreadRetrieveUpdateDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupThread
+        fields = '__all__'
+
+    def validate_name(self, name):
+        if name == self.instance.name:
+            return name
+
+        if self.context['request'].user != self.instance.owner:
+            raise serializers.ValidationError('Only group owner can change a name.')
+
+        exists = GroupThread.objects.filter(
+            owner=self.context['request'].user, name=name
+        ).exclude(
+            id=self.instance.id
+        ).exists()
+
+        if exists:
+            raise serializers.ValidationError('You already have group chat with this name.')
+
+        return name
+
+    def validate_participants(self, participants):
+        # not owner
+        if self.context['request'].user != self.instance.owner:
+            diff = set(self.instance.participants.all()) - set(participants)
+
+            # participant (not owner) can remove only himself
+            if diff and diff != {self.context['request'].user}:
+                raise serializers.ValidationError('Not owner can remove only himself from the group.')
+
+        # owner
+        else:
+            if self.instance.owner not in participants:
+                raise serializers.ValidationError('Owner can not remove himself from his own group.')
+
+        return participants
--- server/api/thread/urls.py
+++ server/api/thread/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
 
-from .views import PrivateThreadListCreateAPIView, GroupThreadListCreateAPIView
+from .views import PrivateThreadListCreateAPIView, PrivateThreadRetrieveDestroyAPIView
+from .views import GroupThreadListCreateAPIView, GroupThreadRetrieveUpdateDestroyAPIView
 
 
 urlpatterns = [
-    url(r'^private/$', PrivateThreadListCreateAPIView.as_view(), name='private_thread_list_create'),
-    url(r'^group/$', GroupThreadListCreateAPIView.as_view(), name='group_thread_list_create'),
+    url(r'^private/$', PrivateThreadListCreateAPIView.as_view(),
+        name='private_thread_list_create'),
+
+    url(r'^private/(?P<pk>[0-9]+)/$', PrivateThreadRetrieveDestroyAPIView.as_view(),
+        name='private_thread_retrieve_destroy'),
+
+    url(r'^group/$', GroupThreadListCreateAPIView.as_view(),
+        name='group_thread_list_create'),
+
+    url(r'^group/(?P<pk>[0-9]+)/$', GroupThreadRetrieveUpdateDestroyAPIView.as_view(),
+        name='group_thread_retrieve_update_destroy'),
 ]
